ad661b7d97682ca3aa6ce489049273d85ff8ab19
Rodas Zegeye <rodaszegeye@Rodass-MacBook-Pro.local>
2021-10-08 13:05:13 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TodoList from "./components/TodoList";
+import { Todo } from "model";
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import InputFeild from "./components/InputFeild";
+import SingleTodo from "./components/SingleTodo";
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+  console.log(todos);
+
+  console.log(todo);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="heading">Taskify</span>
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
+
+      {/* {todos.map((t) => (
+        <li>{t.todo}</li>
+      ))} */}
     </div>
   );
-}
+};
 
 export default App;
